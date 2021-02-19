22d9e2fad967a02fdc8908addf5ffceacabe11cc
Landung "Don" Setiawan <landungs@uw.edu>
2021-02-19 09:13:29 -0800

    Add default status n/a

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ from pathlib import Path
 
 def  main():
     file_path = os.environ.get("INPUT_FILE_PATH")
+    status = 'n/a'
     if file_path:
         path = Path(file_path)
         status_json = yaml.load(path.open(), Loader=yaml.SafeLoader)
-        args = ["echo", "::set-output", f"name=status::{status_json['status']}"]
-        subprocess.call(args)
+        status = status_json['status']
     else:
         print("file_path not found.")
+    
+    args = ["echo", "::set-output", f"name=status::{status}"]
+    subprocess.call(args)
 
 
 if __name__ == "__main__":
